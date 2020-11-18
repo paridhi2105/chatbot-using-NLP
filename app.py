0d6755c0f08106137c91e11fc4799931ececba83
paridhi2105 <55832462+paridhi2105@users.noreply.github.com>
2020-11-19 00:59:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,103 +10,101 @@ import random
 import json
 import pickle
 from flask import Flask,render_template,request
+import os
 
-global model,words,labels,data
+app = Flask(__name__)
 
-app = Flask(__name__) 
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+
+    return numpy.array(bag)
+
+with open("intents.json",encoding='utf-8') as file:
+    data = json.load(file)
+try:
+    with open("data.pickle", "rb") as f:
+        words, labels, training, output = pickle.load(f)
+except:
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
+
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
+
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+    words = sorted(list(set(words)))
+
+    labels = sorted(labels)
+
+    training = []
+    output = []
+
+    out_empty = [0 for _ in range(len(labels))]
+
+    for x, doc in enumerate(docs_x):
+        bag = []
+        wrds = [stemmer.stem(w.lower()) for w in doc]
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
+        training.append(bag)
+        output.append(output_row)
+
+    training = numpy.array(training)
+    output = numpy.array(output)
+
+    with open("data.pickle", "wb") as f:
+        pickle.dump((words, labels, training, output), f)
+
+tf.compat.v1.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+path = "model.tflearn"
+if os.path.isfile(path):
+    model.load(path)
+else:
+    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=False)
+    model.save(path)
+
 
 @app.route('/')
 def index():
      return render_template("index.html")
 
-def mlmodel():
-    global words,model,labels,data
-    with open("intents.json",encoding='utf-8') as file:
-        data = json.load(file)
-    try:
-        with open("data.pickle", "rb") as f:
-            words, labels, training, output = pickle.load(f)
-    except:
-        words = []
-        labels = []
-        docs_x = []
-        docs_y = []
-     
-        for intent in data["intents"]:
-            for pattern in intent["patterns"]:
-                wrds = nltk.word_tokenize(pattern)
-                words.extend(wrds)
-                docs_x.append(wrds)
-                docs_y.append(intent["tag"])
-            if intent["tag"] not in labels:
-                labels.append(intent["tag"])
-                    
-        words = [stemmer.stem(w.lower()) for w in words if w != "?"]
-        words = sorted(list(set(words)))
-    
-        labels = sorted(labels)
-    
-        training = []
-        output = []
-    
-        out_empty = [0 for _ in range(len(labels))]
-    
-        for x, doc in enumerate(docs_x):
-            bag = []
-            wrds = [stemmer.stem(w.lower()) for w in doc]
-            for w in words:
-                if w in wrds:
-                    bag.append(1)
-                else:
-                    bag.append(0)
-            output_row = out_empty[:]
-            output_row[labels.index(docs_y[x])] = 1
-            training.append(bag)
-            output.append(output_row)
-    
-        training = numpy.array(training)
-        output = numpy.array(output)
-    
-        with open("data.pickle", "wb") as f:
-            pickle.dump((words, labels, training, output), f)
-    
-    tf.compat.v1.reset_default_graph()
-    
-    net = tflearn.input_data(shape=[None, len(training[0])])
-    net = tflearn.fully_connected(net, 8)
-    net = tflearn.fully_connected(net, 8)
-    net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
-    net = tflearn.regression(net)
-    
-    model = tflearn.DNN(net)
-    
-    try:
-        model.load("model.tflearn")
-    except:
-        model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
-        model.save("model.tflearn")
-        
-def bag_of_words(s, words):
-    bag = [0 for _ in range(len(words))]
-    s_words = nltk.word_tokenize(s)
-    s_words = [stemmer.stem(word.lower()) for word in s_words]
-    
-    for se in s_words:
-        for i, w in enumerate(words):
-            if w == se:
-                bag[i] = 1
-                    
-    return numpy.array(bag)
-
 @app.route('/get')
 def get_bot_response():
     userText = request.args.get("msg")
-    
+
     if userText.lower() == "quit":
         exit()
- 
-    global model,words,labels,data
-    
+
+    print(model)
     results = model.predict([bag_of_words(userText, words)])
     results_index = numpy.argmax(results)
     tag = labels[results_index]   
@@ -118,5 +116,4 @@ def get_bot_response():
 
 
 if __name__ == '__main__':
-    mlmodel()
     app.run()
